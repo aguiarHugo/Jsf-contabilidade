5b94f3edfe84d7a67102ed0f7df4beee0e0b56f3
aguiarHugo <aguiarhugofx@gmail.com>
2023-03-20 15:23:30 -0300

    Imported BrowserRouter, styles and Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
+import { BrowserRouter } from "react-router-dom"
 
+import { styles } from "./styles/styles"
+import Navbar  from './components/Navbar'
 
 function App() {
   return (
   <div>
-    Olá
+    <BrowserRouter>
+      <div className="relative z-0 bg-black-200">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+        </div>
+        
+        </div>
+    </BrowserRouter>
   </div>
   )
 }
