46f6178c96967614ffdf4be312609e7a5649779d
aguiarHugo <aguiarhugofx@gmail.com>
2023-03-20 15:24:51 -0300

    Created const navlinks

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+export const navLinks = [
+  {
+    id: "whatIs",
+    title: "O que é Contabilidade?",
+  },
+  {
+    id: "about",
+    title: "Quem somos"
+  },
+  {
+    id: "mission",
+    title: "Missão",
+  },
+  {
+    id: "contact",
+    title: "Contato",
+  },
+]
