c2013de24adf16c4a2e32babf9774d2915118e14
aguiarHugo <aguiarhugofx@gmail.com>
2023-03-22 15:40:09 -0300

    Emailjs Keys

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "Cachaça Premiada",
+          to_name: "JSF Contabilidade",
           from_email: form.email,
-          to_email: "premiadacachaca@gmail.com",
+          to_email: "contabilidadejsf@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
