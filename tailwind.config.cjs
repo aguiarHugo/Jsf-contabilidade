af0856413e1703961245cc06334a06e4b9f8927b
aguiarHugo <aguiarhugofx@gmail.com>
2023-03-22 09:50:14 -0300

    Styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,10 +5,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "jsf-blue": "#080671",
+        "jsf-blue": "#1b536c",
+        "jsf-button": "#1b436c",
         "black-100": "#111",
         "black-200": "#1d1d1d",
-        "white-100": "#f3f3f3"
+        "white-100": "#f3f3f3",
+        "white-100": "#f6f6f6",
       },
       boxShadow: {
         card: "0px 35px 120px -15px #211e35",
@@ -19,7 +21,7 @@ module.exports = {
       backgroundImage: {
         "hero-pattern": "url('/src/assets/office2.png')",
 
-        "jsf-gradient": "linear-gradient(to right, #080671 0%, #B8B8B8 100%)"
+        "jsf-gradient": "linear-gradient(to right, #1b536c 0%, #EDED 100%)"
       },
     },
   },
