762d159e51ec5944bc8748ab1b7757bcdb94a28c
aguiarHugo <aguiarhugofx@gmail.com>
2023-03-22 10:29:57 -0300

    Navigation changes with DotGroup

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,38 @@
 import { BrowserRouter } from "react-router-dom"
+import useMediaQuery from "./hooks/useMediaQuery";
+import { useState } from "react"
 
-import { styles } from "./styles/styles"
 import Navbar  from './components/Navbar'
 import Hero from "./components/Hero"
 import Countability from "./components/Countability"
 import About from "./components/About"
 import Contact from "./components/Contact"
+import DotGroup from "./components/DotGroup"
 
 function App() {
+  const isAboveMediumScreens = useMediaQuery("(min-width: 1060px)")
+  const [selectedPage, setSelectedPage] = useState('home')
+
   return (
   <div>
     <BrowserRouter>
-      <div className="relative z-0 bg-white-100">
+      <div id="home" className="relative z-0 bg-white-100">
         <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center bg-black">
-          <Navbar />
+          <Navbar 
+          selectedPage={selectedPage}
+          setSelectedPage={setSelectedPage}
+          />
+          
           <Hero />
+          {isAboveMediumScreens && (
+            <DotGroup 
+              selectedPage={selectedPage}
+              setSelectedPage={setSelectedPage}
+            />
+          )}
+
+        </div>
+        <div>
         </div>
         <div id="countability" className="h-6 md:h-0.5"/>
         <Countability />
