c6efda4fc6cc61dfb9e23b847499a4221132e445
aguiarHugo <aguiarhugofx@gmail.com>
2023-03-23 09:15:53 -0300

    testimonials

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,14 +5,45 @@ export const navLinks = [
   },
   {
     id: "countability",
-    title: "O que é Contabilidade?",
+    title: "Contabilidade",
   },
   {
     id: "about",
     title: "Quem somos"
   },
   {
+    id: "testimonials",
+    title: "Depoimentos"
+  },
+  {
     id: "contact",
     title: "Contato",
   },
 ]
+
+export const testimonials = [
+  {
+    testimonial:
+      "Sem dúvidas, os melhores profissionais disponíveis no mercado atualmente.",
+    name: "Andressa Milhorance",
+    designation: "CFO",
+    company: "Acme Co",
+    image: "https://randomuser.me/api/portraits/women/4.jpg",
+  },
+  {
+    testimonial:
+      "Sempre achei que contadores não se preocupavam com seus clientes, até que a JSF me provou que eu estava errado.",
+    name: "Murilo Pinheiro",
+    designation: "COO",
+    company: "Umbrella Corp",
+    image: "https://randomuser.me/api/portraits/men/5.jpg",
+  },
+  {
+    testimonial:
+      "Só tenho a agradecer a JSF por todo serviço prestado.",
+    name: "Ludmilla Tavares",
+    designation: "CTO",
+    company: "456 Enterprises",
+    image: "https://randomuser.me/api/portraits/women/6.jpg",
+  },
+];
